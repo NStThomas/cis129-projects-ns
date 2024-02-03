14dd554d8b69397c80ff845cb86f169088db05ca
NStThomas <158239643+NStThomas@users.noreply.github.com>
2024-02-03 14:11:25 -0700

    Update cis129_lab02_shortStory.py
    deleted Hello, world! and add a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+# Author: Nancy StThomas
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print('There was once a young girl who loved cookies.') 
+print('She enjoyed her cookies with milk everyday.')
+print('Until she became lactose intoleran!')
+print('Now she no longer enjoys cookies.')
